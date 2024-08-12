9a612ac87138cfb7cf17d4699861d6db2d66e78f
Derek Gooding <djgooding@live.com>
2024-08-12 15:54:49 -0400

    Update MainWindow.xaml.cs

--- Source/MainWindow.xaml.cs
+++ Source/MainWindow.xaml.cs
@@ -1,14 +1,8 @@
 ﻿using System.Windows;
 
 namespace WpfApp1;
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
+
 public partial class MainWindow : Window
 {
-    public MainWindow()
-    {
-        InitializeComponent();
-
-    }
+    public MainWindow() => InitializeComponent();
 }
